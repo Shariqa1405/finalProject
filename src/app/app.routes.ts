1053e9e942e15264b3c4432263ca70b767eb3d1b
Shariqa1405 <revazasharikadze@gmail.com>
2024-11-11 20:30:05 +0400

    updating before angular update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { AuthComponent } from './auth/auth.component';
+import { TemplateComponent } from './template/template.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: AppComponent },
+  { path: 'auth', component: AuthComponent },
+  { path: 'templates', component: TemplateComponent },
+];
